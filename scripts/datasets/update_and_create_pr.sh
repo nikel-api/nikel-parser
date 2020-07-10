9743bfbb8c1e0711b48f7ec5d034740c25231631
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-07-10 08:37:03 -0400

    forgot to use PROJECT_DIR, should be good now

--- scripts/datasets/update_and_create_pr.sh
+++ scripts/datasets/update_and_create_pr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bash scripts/datasets/update_local_data.sh
-bash scripts/datasets/create_new_data_pr.sh
+bash $PROJECT_DIR/nikel-parser/scripts/datasets/update_local_data.sh
+bash $PROJECT_DIR/nikel-parser/scripts/datasets/create_new_data_pr.sh
