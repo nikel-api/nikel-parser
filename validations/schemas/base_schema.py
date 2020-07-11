e0126b4031b90c3d5eb8ae5801ae377e007a2964
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-07-11 11:57:44 -0400

    courses can be longer apparently

--- validations/schemas/base_schema.py
+++ validations/schemas/base_schema.py
@@ -7,6 +7,6 @@ class BaseSchema:
     VALID_CAMPUSES = ['St. George', 'Scarborough', 'Mississauga']
 
     # Lambda Methods
-    COURSE_CODE_LAMBDA = lambda code: 7 <= len(code) <= 9
+    COURSE_CODE_LAMBDA = lambda code: 7 <= len(code) <= 10
 
     # Static Helpers
