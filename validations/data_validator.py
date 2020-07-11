4a08106d4e46569649b0a844308cb29e7b03cbdf
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-07-11 09:58:02 -0400

    helper methods to run validations & some refactoring

--- validations/data_validator.py
+++ validations/data_validator.py
@@ -10,33 +10,60 @@ from schemas.textbooks_schema import TextbooksSchema
 
 class DataValidator:
     json_mapping = {
-        'buildings': { 'file': '../data/buildings.json', 'klass': BuildingsSchema },
-        'courses': '../data/courses.json',
-        'evals': '../data/evals.json',
-        'exams': '../data/exams.json',
-        'food': '../data/food.json',
-        'parking': '../data/parking.json',
-        'services': '../data/services.json',
-        'textbooks': '../data/textbooks.json',
+        'buildings':    {'file': 'data/buildings.json', 'klass': BuildingsSchema},
+        'courses':      {'file': 'data/courses.json', 'klass': CoursesSchema},
+        'evals':        {'file': 'data/evals.json', 'klass': EvalsSchema},
+        'exams':        {'file': 'data/exams.json', 'klass': ExamsSchema},
+        'food':         {'file': 'data/food.json', 'klass': FoodSchema},
+        'parking':      {'file': 'data/parking.json', 'klass': ParkingSchema},
+        'services':     {'file': 'data/services.json', 'klass': ServicesSchema},
+        'textbooks':    {'file': 'data/textbooks.json', 'klass': TextbooksSchema},
     }
 
     def __init__(self):
-        pass
+        self.failed = []
+
+    def run_all_validations(self):
+        mapping = DataValidator.json_mapping
+
+        for data_type in mapping:
+            with open(mapping[data_type]['file'], 'r', encoding='utf8') as f:
+                data = json.load(f)
+                print(f'Validating {data_type}...', end='', flush=True)
+
+                result = self.run_validation(data, mapping[data_type]['klass'])
+
+                # If we get something back, it means it wasn't successful
+                if(result):
+                    self.failed.append({ 'course': data_type, 'error': result })
+                    print('Failed')
+                else:
+                    print('Passed')
+        
+        for failure in self.failed:
+            print("\n" + ('*' * 10))
+            print(f'Validation failed for {failure["course"]}')
+            print(f"Stacktrace:\n{failure['error']}\n")
+
+            
+        print(f'\nValidated {len(mapping)} schemas. {len(mapping) - len(self.failed)} passed, {len(self.failed)} failed.')
+    
+    def run_validation(self, obj, schema):
+        try:
+            DataValidator.validate_with_exception(obj, schema)
+        except BaseException as error:
+            return error
 
     @staticmethod
-    def validate(json_data, schema):
+    def validate_with_exception(json_data, schema):
         return schema.SCHEMA.validate(json_data)
-    
+
     @staticmethod
     def is_valid(json_data, schema):
         return schema.SCHEMA.is_valid(json_data)
 
 if(__name__ == "__main__"):
-
-    with open('data/textbooks.json', 'r', encoding='utf8') as f:
-        data = json.load(f)
-
-        print(DataValidator.validate(data, TextbooksSchema))
-        print(DataValidator.is_valid(data, TextbooksSchema))
+    v = DataValidator()
+    v.run_all_validations()
 
 
